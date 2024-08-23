65a34186f13508087d5384d5a068911a876573f3
bienbeo <tranngocbien2809@gmail.com>
2024-08-23 16:52:02 +0700

    animated-search-bar

---
+++ js-project/animated-search-bar/index.js
@@ -0,0 +1,6 @@
+const magnifierEl = document.querySelector(".magnifier");
+const searchBarEl = document.querySelector(".search-bar-container");
+
+magnifierEl.addEventListener("click", () => {
+    searchBarEl.classList.toggle("active");
+});
